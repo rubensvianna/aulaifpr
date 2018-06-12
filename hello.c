cf1f3bfbeeb333ce9621cd1bab89e5c615e3832a
Rubens Vianna <rubensvianna.rv@gmail.com>
2018-06-11 22:37:31 -0300

    Inclusão de idade

--- hello.c
+++ hello.c
@@ -4,9 +4,11 @@ int main()
 {
 
 char nome[100];
+int idade;
 
 scanf("%s", nome);
+scanf("%i", &idade);
 
-   printf("hello %s\n", nome);
+   printf("hello %s de %i anos\n", nome, idade);
    return 0;
 }
